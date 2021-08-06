f8f4ebfa7b97a48236cfbbcf26ea2f13ad0f9b3b
Carlo Supina <cdsupina@gmail.com>
2021-08-06 14:42:16 -0500

    check for debug_assertions to run for collider debug lines

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,29 @@ mod misc;
 mod player;
 
 fn main() {
-    App::build()
-        .insert_resource(WindowDescriptor {
-            title: "Theta Wave".to_string(),
-            width: 960.0,
-            height: 720.0,
-            ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugin(DebugLinesPlugin)
-        //.add_plugin(RapierRenderPlugin)
-        .add_startup_system(setup_game.system().label("init"))
-        .add_startup_system(misc::spawn_barrier_system.system().after("init"))
-        .add_startup_system(player::spawn_player_system.system().after("init"))
-        .add_system(player::player_movement_system.system())
-        .add_system(debug::collider_debug_lines_system.system())
-        //.add_system(print_player_position.system())
-        .run();
+    let mut app = App::build();
+
+    app.insert_resource(WindowDescriptor {
+        title: "Theta Wave".to_string(),
+        width: 960.0,
+        height: 720.0,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins)
+    .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+    .add_plugin(DebugLinesPlugin)
+    //.add_plugin(RapierRenderPlugin)
+    .add_startup_system(setup_game.system().label("init"))
+    .add_startup_system(misc::spawn_barrier_system.system().after("init"))
+    .add_startup_system(player::spawn_player_system.system().after("init"))
+    .add_system(player::player_movement_system.system());
+    //.add_system(print_player_position.system())
+
+    if cfg!(debug_assertions) {
+        app.add_system(debug::collider_debug_lines_system.system());
+    }
+
+    app.run();
 }
 
 fn setup_game(mut commands: Commands, mut rapier_config: ResMut<RapierConfiguration>) {
