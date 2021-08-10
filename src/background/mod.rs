1b321a3e2d8f06c05cf5d529268b85b7920c59e2
Carlo Supina <cdsupina@gmail.com>
2021-08-09 22:35:22 -0500

    add rotating sun and earth, change camera to perspective

---
+++ src/background/mod.rs
@@ -0,0 +1,52 @@
+use bevy::prelude::Commands;
+use bevy::prelude::*;
+
+pub struct PlanetComponent {
+    rotation_speed: f32,
+}
+
+pub fn rotate_planet_system(mut query: Query<(&mut Transform, &PlanetComponent)>) {
+    for (mut transform, planet) in query.iter_mut() {
+        transform.rotation *= Quat::from_rotation_y(planet.rotation_speed);
+    }
+}
+
+pub fn create_background_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let earth_transform = Transform {
+        translation: Vec3::new(550.0, -300.0, -775.0),
+        scale: Vec3::new(450.0, 450.0, 450.0),
+        rotation: Quat::from_rotation_z(0.41),
+    };
+
+    let sun_transform = Transform {
+        translation: Vec3::new(-1150.0, 300.0, -2000.0),
+        scale: Vec3::new(70.0, 70.0, 70.0),
+        ..Default::default()
+    };
+
+    commands
+        .spawn_bundle((earth_transform, GlobalTransform::identity()))
+        .with_children(|parent| {
+            parent.spawn_bundle(PbrBundle {
+                mesh: asset_server.load("models/earth.glb#Mesh0/Primitive0"),
+                material: asset_server.load("models/earth.glb#Material0"),
+                ..Default::default()
+            });
+        })
+        .insert(PlanetComponent {
+            rotation_speed: 0.0001,
+        });
+
+    commands
+        .spawn_bundle((sun_transform, GlobalTransform::identity()))
+        .with_children(|parent| {
+            parent.spawn_bundle(PbrBundle {
+                mesh: asset_server.load("models/sun.glb#Mesh0/Primitive0"),
+                material: asset_server.load("models/sun.glb#Material0"),
+                ..Default::default()
+            });
+        })
+        .insert(PlanetComponent {
+            rotation_speed: 0.00005,
+        });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::PerspectiveProjection};
 use bevy_prototype_debug_lines::*;
 use bevy_rapier2d::{na::Vector2, prelude::*};
 use ron::de::{from_bytes, from_str};
 use std::{env::current_dir, fs::read_to_string};
 
+mod background;
 mod debug;
 mod game;
 mod misc;
@@ -37,10 +38,12 @@ fn main() {
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(DebugLinesPlugin)
         .add_startup_system(setup_game.system().label("init"))
+        .add_startup_system(background::create_background_system.system().after("init"))
         .add_startup_system(misc::spawn_barrier_system.system().after("init"))
         .add_startup_system(player::spawn_player_system.system().after("init"))
         .add_system(player::player_movement_system.system())
-        .add_system(options::toggle_fullscreen_system.system());
+        .add_system(options::toggle_fullscreen_system.system())
+        .add_system(background::rotate_planet_system.system());
 
     if cfg!(debug_assertions) {
         app.add_system(debug::collider_debug_lines_system.system());
@@ -54,9 +57,15 @@ fn setup_game(
     mut rapier_config: ResMut<RapierConfiguration>,
     game_parameters: Res<game::GameParametersResource>,
 ) {
-    // spawn camera
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
+    // setup camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 950.0).looking_at(Vec3::ZERO, Vec3::Y),
+        perspective_projection: PerspectiveProjection {
+            far: 10000.0,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
     // setup rapier
     rapier_config.gravity = Vector2::zeros();
     rapier_config.scale = game_parameters.physics_scale;
