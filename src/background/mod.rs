bcacc3e770af351a5e7376ac74dbd0f6a653be8a
Carlo Supina <cdsupina@gmail.com>
2021-08-12 02:43:52 -0500

    update earth and sun models, add functions to save and load scene

--- src/background/mod.rs
+++ src/background/mod.rs
@@ -1,12 +1,15 @@
 use bevy::prelude::Commands;
 use bevy::prelude::*;
 
+#[derive(Reflect, Default)]
+#[reflect(Component)]
 pub struct PlanetComponent {
-    rotation_speed: f32,
+    pub rotation_speed: f32,
 }
 
 pub fn rotate_planet_system(mut query: Query<(&mut Transform, &PlanetComponent)>) {
     for (mut transform, planet) in query.iter_mut() {
+        //println!("{:?}", transform.translation);
         transform.rotation *= Quat::from_rotation_y(planet.rotation_speed);
     }
 }
@@ -60,4 +63,14 @@ pub fn create_background_system(mut commands: Commands, asset_server: Res<AssetS
         transform: sun_transform,
         ..Default::default()
     });
+}
+
+pub fn load_background_scene_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut scene_spawner: ResMut<SceneSpawner>,
+) {
+    let scene_handle: Handle<DynamicScene> = asset_server.load("scenes/earth_sun.scn.ron");
+    scene_spawner.spawn_dynamic(scene_handle);
+    asset_server.watch_for_changes().unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use bevy::{pbr::AmbientLight, prelude::*, render::camera::PerspectiveProjection};
+use bevy::{
+    ecs::entity::Entities,
+    pbr::AmbientLight,
+    prelude::*,
+    reflect::TypeRegistry,
+    render::{camera::PerspectiveProjection, pipeline::IndexFormat},
+};
 use bevy_prototype_debug_lines::*;
 use bevy_rapier2d::{na::Vector2, prelude::*};
 use ron::de::{from_bytes, from_str};
-use std::{env::current_dir, fs::read_to_string};
+use std::{
+    env::current_dir,
+    fs::{read_to_string, File},
+    io::Write,
+};
 
 mod background;
 mod debug;
@@ -23,7 +33,8 @@ fn main() {
 
     let mut app = App::build();
 
-    app.insert_resource(WindowDescriptor::from(display_config))
+    app.register_type::<Option<IndexFormat>>()
+        .insert_resource(WindowDescriptor::from(display_config))
         .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(AmbientLight {
             color: Color::WHITE,
@@ -43,12 +54,21 @@ fn main() {
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(DebugLinesPlugin)
         .add_startup_system(setup_game.system().label("init"))
+        /*
+        .add_startup_system(
+            background::load_background_scene_system
+                .system()
+                .after("init"),
+        )
+        */
         .add_startup_system(background::create_background_system.system().after("init"))
+        //.add_startup_system(save_scene_system.exclusive_system().at_end())
         .add_startup_system(misc::spawn_barrier_system.system().after("init"))
         .add_startup_system(player::spawn_player_system.system().after("init"))
         .add_system(player::player_movement_system.system())
         .add_system(options::toggle_fullscreen_system.system())
         .add_system(background::rotate_planet_system.system());
+    //.add_system(print_entities.system());
 
     if cfg!(debug_assertions) {
         app.add_system(debug::collider_debug_lines_system.system());
@@ -75,4 +95,85 @@ fn setup_game(
     // setup rapier
     rapier_config.gravity = Vector2::zeros();
     rapier_config.scale = game_parameters.physics_scale;
+}
+
+fn print_entities(query: Query<Entity, With<background::PlanetComponent>>) {
+    for entity in query.iter() {
+        println!("{:?}", entity);
+    }
+}
+
+fn save_scene_system(world: &mut World) {
+    let earth_transform = Transform {
+        translation: Vec3::new(550.0, -300.0, -775.0),
+        scale: Vec3::new(450.0, 450.0, 450.0),
+        rotation: Quat::from_rotation_z(0.41),
+    };
+
+    let sun_transform = Transform {
+        translation: Vec3::new(-1150.0, -300.0, -2000.0),
+        scale: Vec3::new(70.0, 70.0, 70.0),
+        ..Default::default()
+    };
+
+    let mut scene_world = World::new();
+
+    let asset_server = world.get_resource::<AssetServer>().unwrap();
+    let earth_mesh = asset_server.load("models/earth.glb#Mesh0/Primitive0");
+    let earth_mat = asset_server.load("models/earth.glb#Material0");
+    let sun_mesh = asset_server.load("models/sun.glb#Mesh0/Primitive0");
+    let sun_mat = asset_server.load("models/sun.glb#Material0");
+
+    // add earth entity
+    scene_world
+        .spawn()
+        .insert_bundle((earth_transform, GlobalTransform::identity()))
+        .with_children(|parent| {
+            parent.spawn_bundle(PbrBundle {
+                mesh: earth_mesh,
+                material: earth_mat,
+                ..Default::default()
+            });
+        })
+        .insert(background::PlanetComponent {
+            rotation_speed: 0.0002,
+        });
+
+    scene_world
+        .spawn()
+        .insert_bundle((sun_transform, GlobalTransform::identity()))
+        .with_children(|parent| {
+            parent.spawn_bundle(PbrBundle {
+                mesh: sun_mesh,
+                material: sun_mat,
+                ..Default::default()
+            });
+        })
+        .insert(background::PlanetComponent {
+            rotation_speed: 0.00005,
+        });
+
+    scene_world.spawn().insert_bundle(LightBundle {
+        light: Light {
+            color: Color::ORANGE_RED,
+            intensity: 20000000.0,
+            range: 10000000.0,
+            ..Default::default()
+        },
+        transform: sun_transform,
+        ..Default::default()
+    });
+
+    // The TypeRegistry resource contains information about all registered types (including
+    // components). This is used to construct scenes.
+    let type_registry = world.get_resource::<TypeRegistry>().unwrap();
+    let scene = DynamicScene::from_world(&scene_world, &type_registry);
+
+    let mut file = File::create("assets/scenes/earth_sun.scn.ron").unwrap();
+
+    file.write_all(scene.serialize_ron(&type_registry).unwrap().as_bytes())
+        .unwrap();
+
+    // Scenes can be serialized like this:
+    //info!("{}", scene.serialize_ron(&type_registry).unwrap());
 }
