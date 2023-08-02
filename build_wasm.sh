7709df6bb1de1576a63042db96527bd38bfef779
varoonp123 <pazhyanurv@gmail.com>
2023-08-02 00:01:01 -0500

    First pass at github ci (#60)
    compile to mac+windows+wasm+linux for every PR into main

--- build_wasm.sh
+++ build_wasm.sh
@@ -4,6 +4,7 @@ rm -rf ./out/
 mkdir ./out
 rustup target add wasm32-unknown-unknown
 cargo build --release --target wasm32-unknown-unknown
+cargo install wasm-bindgen-cli
 wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/thetawave.wasm
 cp -R ./wasm-page-template/* ./out/
 cp -R ./assets ./out/
