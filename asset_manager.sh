c37e6597d4c490c84af0cdb778167cc26e6d30a0
Carlo Supina <cdsupina@gmail.com>
2023-08-03 01:17:32 -0500

    add fallbacks for background and planet models

--- asset_manager.sh
+++ asset_manager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -euo pipefail
 if [ "$1" = "get-free" ]; then
     aws s3 cp s3://thetawave-assets/free_assets/planets/ ./assets/models/planets/ --recursive --no-sign-request
     aws s3 cp s3://thetawave-assets/free_assets/backgrounds/ ./assets/texture/backgrounds/ --recursive --no-sign-request
@@ -7,6 +7,6 @@ elif [ "$1" = "get-premium" ]; then
     aws s3 cp s3://thetawave-assets/premium_assets/planets/ ./assets/models/planets/ --recursive
     aws s3 cp s3://thetawave-assets/premium_assets/backgrounds/ ./assets/texture/backgrounds/ --recursive
 elif [ "$1" = "remove" ]; then
-    rm ./assets/models/planets/*
-    rm ./assets/texture/backgrounds/*
+    rm -f ./assets/models/planets/*
+    rm -f ./assets/texture/backgrounds/*
 fi
--- src/background/mod.rs
+++ src/background/mod.rs
@@ -4,7 +4,7 @@ use std::fs;
 
 use bevy::prelude::Commands;
 use bevy::prelude::*;
-use rand::{distributions::uniform::SampleRange, seq::IteratorRandom, Rng};
+use rand::{seq::IteratorRandom, Rng};
 use ron::de::from_bytes;
 use serde::Deserialize;
 use std::ops::Range;
@@ -39,24 +39,42 @@ impl Plugin for BackgroundPlugin {
     }
 }
 
+/// Parameters for procedurally generated 3D level backgrounds
 #[derive(Resource, Deserialize)]
 pub struct BackgroundsResource {
+    /// Intensity increase rate for star explosion effect
     pub star_explode_intensity: f32,
+    /// Position of the quad with the background image
     pub background_transation: Vec3,
+    /// Range of x coordinates of star position
     pub star_position_x_range: Range<f32>,
+    /// Range of z coordinates of star position
     pub star_position_z_range: Range<f32>,
+    /// Position of the planet
     pub planet_translation: Vec3,
-    pub color_range: Range<f32>,
+    /// Range of colors for the star
+    pub star_color_range: Range<f32>,
+    /// Width of the background quad mesh
     pub background_quad_width: f32,
+    /// Height of the background quad mesh
     pub background_quad_height: f32,
+    /// Alpha channel value of the background
     pub background_alpha: f32,
+    /// Radius of the star's icosphere mesh
     pub star_radius: f32,
+    /// Subdivisions of the star's icosphere mesh
     pub star_subdivisions: usize,
+    /// Subdivisions of a fallback planet's icosphere mesh
+    pub planet_subdivisions: usize,
+    /// Range of rotation speeds for the planet
     pub rotation_speed_range: Range<f32>,
+    /// Intensity of the point light child of the star
     pub star_light_intensity: f32,
+    /// Range of the point light child of the star
     pub star_light_range: f32,
 }
 
+/// Resource to track if star explosion is happening
 #[derive(Resource, Default)]
 pub struct StarExplodeResource {
     pub started: bool,
@@ -70,6 +88,7 @@ pub struct PlanetComponent {
     pub rotation_speed: f32,
 }
 
+/// Component to tag star point light
 #[derive(Component)]
 pub struct StarLightComponent;
 
@@ -88,12 +107,14 @@ pub fn on_defeat_star_explode_system(
     time: Res<Time>,
     backgrounds_res: Res<BackgroundsResource>,
 ) {
+    // Check for loss condition from defense objective
     for event in run_end_event_reader.iter() {
         if let RunOutcomeType::Defeat(RunDefeatType::DefenseDestroyed) = event.outcome {
             star_explode_res.started = true;
         }
     }
 
+    // Update star point light intensity if star explosion active
     if star_explode_res.started {
         for mut point_light in point_light_query.iter_mut() {
             point_light.intensity *= backgrounds_res.star_explode_intensity * time.delta_seconds();
@@ -101,7 +122,7 @@ pub fn on_defeat_star_explode_system(
     }
 }
 
-/// Create background from resource
+/// Create a procedurally generated 3D background for a level
 pub fn create_background_system(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -115,7 +136,7 @@ pub fn create_background_system(
 
     let mut rng = rand::thread_rng();
 
-    // positions
+    // Choose random positions for the bodies
     let background_transform = Transform::from_translation(backgrounds_res.background_transation);
     let star_transform = Transform::from_xyz(
         rng.gen_range(backgrounds_res.star_position_x_range.clone()),
@@ -124,73 +145,145 @@ pub fn create_background_system(
     );
     let planet_transform = Transform::from_translation(backgrounds_res.planet_translation);
 
-    // randomly generate attributes
-    let random_planet_file = fs::read_dir("./assets/models/planets")
-        .unwrap()
-        .choose(&mut rng)
-        .unwrap()
-        .unwrap()
-        .path();
-    let random_planet_filename = random_planet_file.file_name().unwrap().to_str().unwrap();
+    // Choose a random planet or fallback to a white icosphere
+    {
+        let mut planet_commands = commands.spawn_empty();
+        planet_commands
+            .insert(PlanetComponent {
+                rotation_speed: rng.gen_range(backgrounds_res.rotation_speed_range.clone()),
+            })
+            .insert(GameCleanup)
+            .insert(Visibility::default())
+            .insert(ComputedVisibility::default())
+            .insert(Name::new("Planet"));
 
-    let random_background_file = fs::read_dir("./assets/texture/backgrounds")
-        .unwrap()
-        .choose(&mut rng)
-        .unwrap()
-        .unwrap()
-        .path();
-    let random_background_filename = random_background_file
-        .file_name()
-        .unwrap()
-        .to_str()
-        .unwrap();
+        match fs::read_dir("./assets/models/planets")
+            .expect("Failed to access ./assets/models/planets")
+            .choose(&mut rng)
+        {
+            Some(planet_dir_result) => {
+                let random_planet_path = format!(
+                    "models/planets/{}#Scene0",
+                    planet_dir_result
+                        .unwrap()
+                        .path()
+                        .file_name()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                );
 
-    let random_background_path = format!("texture/backgrounds/{random_background_filename}",);
-    let random_planet_path = format!("models/planets/{random_planet_filename}#Scene0");
-    let star_color = Color::rgb_linear(
-        rng.gen_range(backgrounds_res.color_range.clone()),
-        rng.gen_range(backgrounds_res.color_range.clone()),
-        rng.gen_range(backgrounds_res.color_range.clone()),
-    );
+                let planet_model_handle: Handle<Scene> = asset_server.load(random_planet_path);
 
-    //background
-    let background_texture_handle = asset_server.load(random_background_path);
+                planet_commands.insert(SceneBundle {
+                    scene: planet_model_handle,
+                    transform: planet_transform,
+                    ..default()
+                });
+            }
+            None => {
+                let planet_mesh = meshes.add(
+                    shape::Icosphere {
+                        radius: 10.0,
+                        subdivisions: backgrounds_res.planet_subdivisions,
+                    }
+                    .try_into()
+                    .unwrap(),
+                );
 
-    let quad_width = 375.0;
+                planet_commands.insert(PbrBundle {
+                    mesh: planet_mesh,
+                    material: materials.add(StandardMaterial {
+                        base_color: Color::WHITE,
+                        ..default()
+                    }),
+                    transform: planet_transform,
+                    ..default()
+                });
+            }
+        }
+    }
+
+    // Spawn a quad textured with a random background image
+    // Create a quad mesh for the background
     let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
         backgrounds_res.background_quad_width,
         backgrounds_res.background_quad_height,
     ))));
 
-    // this material renders the texture normally
-    let material_handle = materials.add(StandardMaterial {
-        base_color_texture: Some(background_texture_handle),
-        alpha_mode: AlphaMode::Blend,
-        base_color: Color::default().with_a(backgrounds_res.background_alpha),
-        unlit: true,
-        ..default()
-    });
-
-    // textured quad - normal
-    commands
-        .spawn(PbrBundle {
-            mesh: quad_handle,
-            material: material_handle,
-            transform: background_transform,
-            ..default()
-        })
+    // Choose a random background or fallback to a black color
+    let mut background_commands = commands.spawn_empty();
+    background_commands
         .insert(GameCleanup)
         .insert(Visibility::default())
         .insert(ComputedVisibility::default())
-        .insert(Name::new("Space Background"));
+        .insert(Name::new("Space Background"))
+        .insert(
+            match fs::read_dir("./assets/texture/backgrounds")
+                .expect("Failed to access ./assets/texture/backgrounds")
+                .choose(&mut rng)
+            {
+                Some(background_dir_result) => {
+                    let random_background_path = format!(
+                        "texture/backgrounds/{}",
+                        background_dir_result
+                            .unwrap()
+                            .path()
+                            .file_name()
+                            .unwrap()
+                            .to_str()
+                            .unwrap()
+                    );
 
-    // star
-    let material_emissive1 = materials.add(StandardMaterial {
-        emissive: star_color, // 4. Put something bright in a dark environment to see the effect
+                    let background_texture_handle = asset_server.load(random_background_path);
+
+                    let material_handle = materials.add(StandardMaterial {
+                        base_color_texture: Some(background_texture_handle),
+                        alpha_mode: AlphaMode::Blend,
+                        base_color: Color::default().with_a(backgrounds_res.background_alpha),
+                        unlit: true,
+                        ..default()
+                    });
+
+                    PbrBundle {
+                        mesh: quad_handle,
+                        material: material_handle,
+                        transform: background_transform,
+                        ..default()
+                    }
+                }
+                None => {
+                    let material_handle = materials.add(StandardMaterial {
+                        base_color: Color::BLACK,
+                        unlit: true,
+                        ..default()
+                    });
+
+                    PbrBundle {
+                        mesh: quad_handle,
+                        material: material_handle,
+                        transform: background_transform,
+                        ..default()
+                    }
+                }
+            },
+        );
+
+    // Spawn a star with a random color
+    let star_color = Color::rgb_linear(
+        rng.gen_range(backgrounds_res.star_color_range.clone()),
+        rng.gen_range(backgrounds_res.star_color_range.clone()),
+        rng.gen_range(backgrounds_res.star_color_range.clone()),
+    );
+
+    // Emissive colored star material for bloom
+    let star_material = materials.add(StandardMaterial {
+        emissive: star_color,
         ..default()
     });
 
-    let mesh = meshes.add(
+    // Spherical star mesh
+    let star_mesh = meshes.add(
         shape::Icosphere {
             radius: backgrounds_res.star_radius,
             subdivisions: backgrounds_res.star_subdivisions,
@@ -199,47 +292,30 @@ pub fn create_background_system(
         .unwrap(),
     );
 
+    // Spawn the star with a child point light of the same color
     commands
         .spawn((PbrBundle {
-            mesh: mesh.clone(),
-            material: material_emissive1,
+            mesh: star_mesh,
+            material: star_material,
             transform: star_transform,
             ..default()
         },))
         .insert(GameCleanup)
         .insert(Visibility::default())
         .insert(ComputedVisibility::default())
-        .insert(Name::new("Star"));
-
-    // planet
-    let planet_model_handle: Handle<Scene> = asset_server.load(random_planet_path);
-
-    commands
-        .spawn(SceneBundle {
-            scene: planet_model_handle,
-            transform: planet_transform,
-            ..default()
-        })
-        .insert(PlanetComponent {
-            rotation_speed: rng.gen_range(backgrounds_res.rotation_speed_range.clone()),
-        })
-        .insert(GameCleanup)
-        .insert(Visibility::default())
-        .insert(ComputedVisibility::default())
-        .insert(Name::new("Planet"));
-
-    commands
-        .spawn(PointLightBundle {
-            point_light: PointLight {
-                color: star_color,
-                intensity: backgrounds_res.star_light_intensity,
-                range: backgrounds_res.star_light_range,
-                ..Default::default()
-            },
-            transform: star_transform,
-            ..Default::default()
-        })
-        .insert(GameCleanup)
-        .insert(StarLightComponent)
-        .insert(Name::new("Star Point Light"));
+        .insert(Name::new("Star"))
+        .with_children(|parent| {
+            parent
+                .spawn(PointLightBundle {
+                    point_light: PointLight {
+                        color: star_color,
+                        intensity: backgrounds_res.star_light_intensity,
+                        range: backgrounds_res.star_light_range,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                .insert(StarLightComponent)
+                .insert(Name::new("Star Point Light"));
+        });
 }
