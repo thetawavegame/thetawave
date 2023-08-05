ba5b12697dd6c70617e73648c87bc4efa36f143f
Carlo Supina <cdsupina@gmail.com>
2023-08-04 21:58:36 -0500

    adjust asset manager script to remove directories of planets and backgrounds

--- asset_manager.sh
+++ asset_manager.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 if [ "$1" = "get-free" ]; then
     aws s3 cp s3://thetawave-assets/free_assets/planets/ ./assets/models/planets/ --recursive --no-sign-request
@@ -7,6 +7,6 @@ elif [ "$1" = "get-premium" ]; then
     aws s3 cp s3://thetawave-assets/premium_assets/planets/ ./assets/models/planets/ --recursive
     aws s3 cp s3://thetawave-assets/premium_assets/backgrounds/ ./assets/texture/backgrounds/ --recursive
 elif [ "$1" = "remove" ]; then
-    rm -f ./assets/models/planets/*
-    rm -f ./assets/texture/backgrounds/*
+    rm -rf ./assets/models/planets
+    rm -rf ./assets/texture/backgrounds
 fi
