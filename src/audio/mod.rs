363fd679ea99f6da7f707430aedec0fa1408c21f
Carlo Supina <cdsupina@gmail.com>
2023-08-29 10:30:42 -0500

    fix ci error

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_kira_audio::prelude::*;
 use thetawave_interface::{
-    audio::{BGMusicType, ChangeBackgroundMusicEvent, PlaySoundEffectEvent, SoundEffectType},
+    audio::{ChangeBackgroundMusicEvent, PlaySoundEffectEvent},
     states::AppStates,
 };
 
