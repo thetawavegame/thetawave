e584e33c2387523a50f0f456f1838c7e1988d617
Carlo Supina <cdsupina@gmail.com>
2023-09-21 21:56:29 -0500

    Fix a test that we broke because it now requires the input plugins because they are used by the PlayerPlugin

--- src/game/counters/plugin.rs
+++ src/game/counters/plugin.rs
@@ -191,11 +191,14 @@ mod test {
     use crate::game::counters::plugin::CountingMetricsPlugin;
     use crate::player::{CharactersResource, PlayerPlugin};
     use crate::spawnable::SpawnProjectileEvent;
+    use bevy::input::InputPlugin;
     use bevy::prelude::{App, Component, Events};
+    use leafwing_input_manager::prelude::ActionState;
     use thetawave_interface::character::{Character, CharacterType};
     use thetawave_interface::game::historical_metrics::{
         MobKillsByPlayerForCurrentGame, UserStatsByPlayerForCurrentGameCache, DEFAULT_USER_ID,
     };
+    use thetawave_interface::options::input::PlayerAction;
     use thetawave_interface::player::PlayerComponent;
     use thetawave_interface::spawnable::{
         EnemyMobType, Faction, MobDestroyedEvent, MobType, ProjectileType,
@@ -210,7 +213,8 @@ mod test {
             .add_event::<MobDestroyedEvent>()
             .add_event::<SpawnProjectileEvent>()
             .insert_resource(UserStatsByPlayerForCurrentGameCache::default())
-            .add_plugins((PlayerPlugin, CountingMetricsPlugin));
+            .insert_resource(bevy::time::Time::default())
+            .add_plugins((InputPlugin, PlayerPlugin, CountingMetricsPlugin));
         app
     }
     #[derive(Component, Default, Copy, Clone)]
