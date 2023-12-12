37f0ad181c783e33029f35e41aa95731c302effc
Carlo Supina <cdsupina@gmail.com>
2023-12-11 23:32:03 -0600

    change wasm script.js to thetawave-init.js

---
+++ wasm-page-template/thetawave-init.js
@@ -0,0 +1,26 @@
+import init from "./thetawave.js";
+
+// prevent right click
+document.addEventListener("contextmenu", (event) =>
+    event.preventDefault(),
+);
+// Chrome and firefox do not want sound starting unless there is a user gesture. Hence, we start the game onClick
+const initGameButton = document.getElementById("trigger_game_button");
+initGameButton.onclick = () => {
+  // Permit the browser to play audio
+  const audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)();
+  // Check if the AudioContext is suspended
+  if (audioContext.state === "suspended") {
+    audioContext.resume().then(() => {
+      console.log("AudioContext resumed successfully.");
+    });
+  }
+  // Hide the screen so we can play the game.
+  const screenElements = Array.from(document.getElementsByClassName("screen"));
+  console.log(screenElements)
+  screenElements.forEach((element) => {
+    element.style.display = "none";
+  });
+  init();
+};
