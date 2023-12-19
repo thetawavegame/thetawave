6631c886318fa002173e3ce948e9ce9c8b0bbf38
Carlo Supina <cdsupina@gmail.com>
2023-12-18 21:02:38 -0600

    comment MobDestroyedEvent

--- crates/thetawave_interface/src/spawnable.rs
+++ crates/thetawave_interface/src/spawnable.rs
@@ -198,6 +198,8 @@ pub enum TextEffectType {
     ConsumableCollected(ConsumableType),
 }
 
+/// Sends information about destroyed mobs
+/// Includes information that is lost when the entity is destroyed
 #[derive(Event)]
 pub struct MobDestroyedEvent {
     pub mob_type: MobType,
