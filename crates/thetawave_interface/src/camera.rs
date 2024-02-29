2d24c1a0238e872568e027f5fdd5e77eca173db2
Varoon Pazhyanur <pazhyanurv@gmail.com>
2024-02-28 21:23:35 -0600

    add a comment to the screen shake interface

--- crates/thetawave_interface/src/camera.rs
+++ crates/thetawave_interface/src/camera.rs
@@ -1,6 +1,8 @@
 use bevy_ecs::event::Event;
 
+/// An event that triggers the user's screen/camera to shake, as if the player's ship was just hit.
 #[derive(Event)]
 pub struct ScreenShakeEvent {
+    /// This should be between 0 and 1. 0 is no screen shake; 1 is a very aggressive shake.
     pub trauma: f32,
 }
