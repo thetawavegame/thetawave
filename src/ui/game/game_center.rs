8749e3c84dcd42c73962441994ef48978ac2d939
Carlo Supina <cdsupina@gmail.com>
2024-03-05 14:21:27 -0600

    add new ui files

---
+++ src/ui/game/game_center.rs
@@ -0,0 +1,107 @@
+use crate::run::CurrentRunProgressResource;
+use bevy::{
+    asset::Handle,
+    ecs::{
+        component::Component,
+        event::EventReader,
+        query::With,
+        system::{Query, Res},
+    },
+    hierarchy::ChildBuilder,
+    render::color::Color,
+    text::{Font, JustifyText, Text, TextStyle},
+    time::{Time, Timer, TimerMode},
+    ui::{node_bundles::TextBundle, BackgroundColor, Style},
+    utils::default,
+};
+use std::time::Duration;
+use thetawave_interface::run::CyclePhaseEvent;
+
+use super::parent::GameCenterUiChildBuilderExt;
+
+const BASE_TEXT_ALPHA: f32 = 1.0;
+const BASE_BACKGROUND_ALPHA: f32 = 0.4;
+const FONT_SIZE: f32 = 90.0;
+const TEXT_COLOR: Color = Color::WHITE;
+const BACKGROUND_COLOR: Color = Color::BLACK;
+const DEFAULT_FADE_TIME: f32 = 5.0;
+
+#[derive(Component)]
+pub struct CenterTextUi;
+
+#[derive(Component)]
+pub struct FadeOutUiComponent {
+    pub timer: Timer,
+}
+
+impl Default for FadeOutUiComponent {
+    fn default() -> Self {
+        let mut timer = Timer::from_seconds(DEFAULT_FADE_TIME, TimerMode::Once);
+        timer.set_elapsed(Duration::from_secs_f32(DEFAULT_FADE_TIME));
+
+        Self { timer }
+    }
+}
+
+impl GameCenterUiChildBuilderExt for ChildBuilder<'_> {
+    fn spawn_game_center_ui(&mut self, font: Handle<Font>) {
+        self.spawn(TextBundle {
+            style: Style::default(),
+            text: Text::from_section(
+                "",
+                TextStyle {
+                    font: font.clone(),
+                    font_size: FONT_SIZE,
+                    color: TEXT_COLOR,
+                },
+            )
+            .with_justify(JustifyText::Center),
+            background_color: BACKGROUND_COLOR.with_a(0.0).into(),
+            ..default()
+        })
+        .insert(CenterTextUi)
+        .insert(FadeOutUiComponent::default());
+    }
+}
+
+pub fn update_center_text_ui_system(
+    mut cycle_phase_event_reader: EventReader<CyclePhaseEvent>,
+    run_resource: Res<CurrentRunProgressResource>,
+    mut center_text_query: Query<
+        (&mut Text, &mut BackgroundColor, &mut FadeOutUiComponent),
+        With<CenterTextUi>,
+    >,
+) {
+    // if phase has been cycled update the text
+    if cycle_phase_event_reader.read().next().is_some() {
+        if let Some(level) = &run_resource.current_level {
+            if let Some(phase) = &level.current_phase {
+                if let Ok((mut text, mut bg_color, mut fade_out)) =
+                    center_text_query.get_single_mut()
+                {
+                    if let Some(intro_text) = phase.intro_text.clone() {
+                        text.sections[0].value = intro_text;
+                        *bg_color = BACKGROUND_COLOR.with_a(BASE_BACKGROUND_ALPHA).into();
+                        fade_out.timer.reset();
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn text_fade_out_system(
+    mut background_color_query: Query<(&mut Text, &mut BackgroundColor, &mut FadeOutUiComponent)>,
+    time: Res<Time>,
+) {
+    for (mut text, mut bg_color, mut fade_out) in background_color_query.iter_mut() {
+        fade_out.timer.tick(time.delta());
+
+        *bg_color = BACKGROUND_COLOR
+            .with_a(BASE_BACKGROUND_ALPHA * fade_out.timer.fraction_remaining())
+            .into();
+
+        text.sections[0].style.color =
+            TEXT_COLOR.with_a(BASE_TEXT_ALPHA * fade_out.timer.fraction_remaining());
+    }
+}
---
+++ src/ui/game/level.rs
@@ -0,0 +1,138 @@
+use crate::run::CurrentRunProgressResource;
+use bevy::{
+    asset::Handle,
+    ecs::{
+        component::Component,
+        entity::Entity,
+        query::With,
+        system::{Commands, Query, Res},
+    },
+    hierarchy::{BuildChildren, ChildBuilder, DespawnRecursiveExt},
+    render::color::Color,
+    text::{Font, Text, TextStyle},
+    ui::{
+        node_bundles::{NodeBundle, TextBundle},
+        AlignItems, FlexDirection, JustifyContent, Style, UiRect, Val,
+    },
+    utils::default,
+};
+
+use super::parent::LevelUiChildBuilderExt;
+
+const NODE_WIDTH: Val = Val::Percent(50.0);
+const TEXT_COLOR: Color = Color::WHITE;
+const FONT_SIZE: f32 = 48.0;
+const LEVEL_DATA_PADDING: UiRect =
+    UiRect::new(Val::Vw(1.0), Val::Vw(1.0), Val::Vh(2.0), Val::Vh(2.0));
+
+/// Used for querying UI for displaying name
+#[derive(Component)]
+pub struct LevelNameUi;
+
+/// Used for querying UI for displaying level information
+#[derive(Component)]
+pub struct LevelDataUi;
+
+impl LevelUiChildBuilderExt for ChildBuilder<'_> {
+    fn spawn_level_ui(&mut self, font: Handle<Font>) {
+        self.spawn(NodeBundle {
+            style: Style {
+                width: NODE_WIDTH,
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|left| {
+            left.spawn(TextBundle {
+                style: Style::default(),
+                text: Text::from_section(
+                    "",
+                    TextStyle {
+                        font: font.clone(),
+                        font_size: FONT_SIZE,
+                        color: TEXT_COLOR,
+                    },
+                ),
+                ..default()
+            })
+            .insert(LevelNameUi);
+        });
+
+        self.spawn(NodeBundle {
+            style: Style {
+                width: NODE_WIDTH,
+                height: Val::Percent(100.0),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|right| {
+            right
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        padding: LEVEL_DATA_PADDING,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .insert(LevelDataUi);
+        });
+    }
+}
+
+pub fn update_level_ui_system(
+    mut commands: Commands,
+    level_data_ui_query: Query<Entity, With<LevelDataUi>>,
+    mut level_name_ui_query: Query<&mut Text, With<LevelNameUi>>,
+    run_resource: Res<CurrentRunProgressResource>,
+) {
+    if let Some(current_level) = &run_resource.current_level {
+        if let Ok(mut text) = level_name_ui_query.get_single_mut() {
+            text.sections[0].value = current_level.get_name();
+        }
+
+        if let Ok(entity) = level_data_ui_query.get_single() {
+            commands.entity(entity).despawn_descendants();
+
+            if let Some(objective) = &current_level.objective {
+                match objective {
+                    thetawave_interface::objective::Objective::Defense(defense_data) => {
+                        commands.entity(entity).with_children(|level_data_ui| {
+                            level_data_ui
+                                .spawn(NodeBundle {
+                                    style: Style {
+                                        width: Val::Percent(80.0),
+                                        height: Val::Percent(60.0),
+                                        flex_direction: FlexDirection::Row,
+                                        ..default()
+                                    },
+                                    background_color: Color::BLUE.with_a(0.05).into(),
+                                    ..default()
+                                })
+                                .with_children(|defense_ui| {
+                                    defense_ui.spawn(NodeBundle {
+                                        style: Style {
+                                            width: Val::Percent(
+                                                100.0 * defense_data.get_percentage(),
+                                            ),
+                                            height: Val::Percent(100.0),
+                                            ..default()
+                                        },
+                                        background_color: Color::BLUE.with_a(0.75).into(),
+                                        ..default()
+                                    });
+                                });
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
