8a905bec1878bf7e7f0669fa2788c2655480d964
Carlo Supina <cdsupina@gmail.com>
2024-05-01 23:08:44 -0500

    create skeleton of player join ui part 2

---
+++ src/ui/character_selection/mod.rs
@@ -0,0 +1,266 @@
+//! Systems to spawn and style the character selection screen, where each player picks a character
+//! from one of a few options, and possibly enables/diables the tutorial.
+use crate::{game::GameParametersResource, options::PlayingOnArcadeResource};
+
+use super::BouncingPromptComponent;
+use bevy::{
+    asset::AssetServer,
+    ecs::{
+        component::Component,
+        event::{EventReader, EventWriter},
+        query::With,
+        system::{Commands, ParamSet, Query, Res, ResMut},
+    },
+    hierarchy::{BuildChildren, Children},
+    input::gamepad::GamepadButtonChangedEvent,
+    render::{color::Color, view::Visibility},
+    time::{Timer, TimerMode},
+    ui::{
+        node_bundles::{ImageBundle, NodeBundle},
+        AlignItems, BackgroundColor, Display, FlexDirection, JustifyContent, Style, UiRect, Val,
+    },
+};
+use leafwing_input_manager::prelude::ActionState;
+use thetawave_interface::input::{MenuAction, MenuExplorer};
+use thetawave_interface::{
+    character::CharacterType,
+    character_selection::PlayerJoinEvent,
+    player::{PlayerData, PlayerInput, PlayersResource},
+    states::CharacterSelectionCleanup,
+};
+
+mod player_join;
+
+#[derive(Component)]
+pub(super) struct Player1JoinPrompt;
+
+#[derive(Component)]
+pub(super) struct Player1CharacterSelection;
+
+#[derive(Component)]
+pub(super) struct Player2JoinPrompt;
+
+#[derive(Component)]
+pub(super) struct Player2CharacterSelection;
+
+#[derive(Component)]
+pub(super) struct CharacterSelectionChoice {
+    pub character: CharacterType,
+    pub is_active: bool,
+}
+
+#[derive(Component)]
+pub(super) struct CharacterDescription {
+    pub character: Option<CharacterType>,
+}
+
+#[derive(Component)]
+pub(super) struct Player1Description;
+
+#[derive(Component)]
+pub(super) struct Player2Description;
+
+#[derive(Component)]
+pub(super) struct StartGamePrompt;
+
+/// Setup the character selection UI
+pub(super) fn setup_character_selection_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    game_params_res: Res<GameParametersResource>,
+    playing_on_arcade: Res<PlayingOnArcadeResource>,
+) {
+    // Main node containing all character selection ui
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::Column,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(CharacterSelectionCleanup)
+        .with_children(|parent| {
+            // Player selection ui
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(85.0),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        flex_direction: FlexDirection::Column,
+                        padding: UiRect::all(Val::Percent(5.0)),
+                        ..Default::default()
+                    },
+                    background_color: Color::rgba(0.0, 0.0, 1.0, 0.05).into(), // TODO: remove
+                    ..Default::default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                width: Val::Percent(100.0),
+                                height: Val::Percent(50.0),
+                                justify_content: JustifyContent::Center,
+                                flex_direction: FlexDirection::Row,
+                                ..Default::default()
+                            },
+                            background_color: Color::rgba(1.0, 0.0, 0.0, 0.05).into(), // TODO: remove
+                            ..Default::default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    max_width: Val::Percent(50.0),
+                                    min_width: Val::Percent(48.0),
+                                    max_height: Val::Percent(100.0),
+                                    min_height: Val::Percent(90.0),
+                                    justify_content: JustifyContent::Center,
+                                    align_items: AlignItems::Center,
+                                    margin: UiRect {
+                                        left: Val::Vw(0.0),
+                                        right: Val::Vw(2.0),
+                                        top: Val::Vh(0.0),
+                                        bottom: Val::Vh(2.0),
+                                    },
+                                    ..Default::default()
+                                },
+                                background_color: Color::rgba(0.0, 1.0, 0.0, 0.05).into(), // TODO: remove
+                                ..Default::default()
+                            });
+                            if game_params_res.get_max_players() > 1 {
+                                parent.spawn(NodeBundle {
+                                    style: Style {
+                                        max_width: Val::Percent(50.0),
+                                        min_width: Val::Percent(48.0),
+                                        max_height: Val::Percent(100.0),
+                                        min_height: Val::Percent(90.0),
+                                        justify_content: JustifyContent::Center,
+                                        align_items: AlignItems::Center,
+                                        margin: UiRect {
+                                            left: Val::Vw(2.0),
+                                            right: Val::Vw(0.0),
+                                            top: Val::Vh(0.0),
+                                            bottom: Val::Vh(2.0),
+                                        },
+                                        ..Default::default()
+                                    },
+                                    background_color: Color::rgba(0.0, 1.0, 0.0, 0.05).into(), // TODO: remove
+                                    ..Default::default()
+                                });
+                            }
+                        });
+                    // spawn 2 rows if there are 3 or 4 players
+
+                    if game_params_res.get_max_players() > 2 {
+                        parent
+                            .spawn(NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    height: Val::Percent(50.0),
+                                    justify_content: JustifyContent::Center,
+                                    ..Default::default()
+                                },
+                                background_color: Color::rgba(1.0, 0.0, 0.0, 0.05).into(), // TODO: remove
+                                ..Default::default()
+                            })
+                            .with_children(|parent| {
+                                parent.spawn(NodeBundle {
+                                    style: Style {
+                                        max_width: Val::Percent(50.0),
+                                        min_width: Val::Percent(48.0),
+                                        max_height: Val::Percent(100.0),
+                                        min_height: Val::Percent(90.0),
+                                        justify_content: JustifyContent::Center,
+                                        align_items: AlignItems::Center,
+                                        margin: UiRect {
+                                            left: Val::Vw(0.0),
+                                            right: Val::Vw(2.0),
+                                            top: Val::Vh(2.0),
+                                            bottom: Val::Vh(0.0),
+                                        },
+                                        ..Default::default()
+                                    },
+                                    background_color: Color::rgba(0.0, 1.0, 0.0, 0.05).into(), // TODO: remove
+                                    ..Default::default()
+                                });
+                                if game_params_res.get_max_players() > 3 {
+                                    parent.spawn(NodeBundle {
+                                        style: Style {
+                                            max_width: Val::Percent(50.0),
+                                            min_width: Val::Percent(48.0),
+                                            max_height: Val::Percent(100.0),
+                                            min_height: Val::Percent(90.0),
+                                            justify_content: JustifyContent::Center,
+                                            align_items: AlignItems::Center,
+                                            margin: UiRect {
+                                                left: Val::Vw(2.0),
+                                                right: Val::Vw(0.0),
+                                                top: Val::Vh(2.0),
+                                                bottom: Val::Vh(0.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                        background_color: Color::rgba(0.0, 1.0, 0.0, 0.05).into(), // TODO: remove
+                                        ..Default::default()
+                                    });
+                                }
+                            });
+                    }
+                });
+
+            // ui to place button to start the game
+            parent.spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(15.0),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..Default::default()
+                },
+                background_color: Color::rgba(0.0, 1.0, 0.0, 0.05).into(), // TODO: remove
+                ..Default::default()
+            });
+        });
+}
+
+/// Handles players joining the game
+pub(super) fn player_join_system(
+    menu_input_query: Query<&ActionState<MenuAction>, With<MenuExplorer>>,
+    mut gamepad_events: EventReader<GamepadButtonChangedEvent>,
+    mut players_resource: ResMut<PlayersResource>,
+    mut ui_queries: ParamSet<(
+        Query<&mut Style, With<Player1JoinPrompt>>,
+        Query<&mut Style, With<Player2JoinPrompt>>,
+        Query<&mut Style, With<Player1CharacterSelection>>,
+        Query<&mut Style, With<Player2CharacterSelection>>,
+        Query<&mut Visibility, With<StartGamePrompt>>,
+    )>,
+    mut player_join_event: EventWriter<PlayerJoinEvent>,
+) {
+}
+
+// handle the character selection for each player
+pub(super) fn select_character_system(
+    menu_input_query: Query<&ActionState<MenuAction>, With<MenuExplorer>>,
+    mut gamepad_events: EventReader<GamepadButtonChangedEvent>,
+    mut players_resource: ResMut<PlayersResource>,
+    player_1_selection: Query<&Children, With<Player1CharacterSelection>>,
+    player_2_selection: Query<&Children, With<Player2CharacterSelection>>,
+
+    mut character_description_queries: ParamSet<(
+        Query<(&mut Style, &CharacterDescription), With<Player1Description>>,
+        Query<(&mut Style, &CharacterDescription), With<Player2Description>>,
+    )>,
+    mut selection_choice: Query<(
+        &mut CharacterSelectionChoice,
+        &mut BouncingPromptComponent,
+        &mut BackgroundColor,
+    )>,
+) {
+}
---
+++ src/ui/character_selection/player_join.rs
@@ -0,0 +1,14 @@
+use bevy::hierarchy::ChildBuilder;
+/*
+pub(super) trait UIChildBuilderExt {
+    //fn spawn_player_join_row();
+
+    fn spawn_player_join(&mut self);
+}
+
+impl UIChildBuilderExt for ChildBuilder<'_> {
+    fn spawn_player_join(&mut self) {
+        self.spawn()
+    }
+}
+*/
